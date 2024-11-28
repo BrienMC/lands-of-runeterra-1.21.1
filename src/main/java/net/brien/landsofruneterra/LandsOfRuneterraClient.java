6bdd003998dbb844d14bc1f473cf5a13345c3e4e
BrienMC <brienmcasayuran@gmail.com>
2024-11-28 14:15:37 -0700

    Initial commit

---
+++ src/main/java/net/brien/landsofruneterra/LandsOfRuneterraClient.java
@@ -0,0 +1,10 @@
+package net.brien.landsofruneterra;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LandsOfRuneterraClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/brien/landsofruneterra/LandsOfRuneterraDataGenerator.java
@@ -0,0 +1,11 @@
+package net.brien.landsofruneterra;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LandsOfRuneterraDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
