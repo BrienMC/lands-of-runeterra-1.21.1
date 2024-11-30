c32d41ab6ce92e28f9645bd257af1322fe1abcb8
BrienMC <brienmcasayuran@gmail.com>
2024-11-30 08:36:33 -0700

    Hexite Ore Functionality - Both Hexite Ores can now be mined with a Diamond Pickaxe or above and drop Unstable Hexite

--- src/main/java/net/brien/landsofruneterra/block/ModBlocks.java
+++ src/main/java/net/brien/landsofruneterra/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.brien.landsofruneterra.LandsOfRuneterra;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,6 +12,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
 
@@ -19,8 +21,12 @@ public class ModBlocks {
     // Keep this for placeholder to copy and paste
     // public static final Block HEXITE_ORE = registerBlock("hexite_ore", new Block(AbstractBlock.Settings.create().strength(3f).requiresTool()));
 
-    public static final Block HEXITE_ORE = registerBlock("hexite_ore", new Block(AbstractBlock.Settings.create().strength(3f).requiresTool()));
-    public static final Block HEXITE_DEEPSLATE_ORE = registerBlock("hexite_deepslate_ore", new Block(AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+    public static final Block HEXITE_ORE = registerBlock("hexite_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+    public static final Block HEXITE_DEEPSLATE_ORE = registerBlock("hexite_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
 
     // Helper method for setting up new blocks in the world
